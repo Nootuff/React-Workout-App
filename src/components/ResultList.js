fa939d69fdcb2558cd06628eb5b0134acdc0cbf4
Adam <adamwalkerlondon@gmail.com>
2022-01-03 10:52:21 +0000

    Axios request now runs with user inputted search-term.

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,50 +1,50 @@
 import React, { useState } from 'react';
-import SearchForm from "./SearchForm";
-const axios = require('axios').default;
 
 function ResultList(props) {
 
-    const [data, setData] = useState("");
-    const [searchTerm, setSearchTerm] = useState("");
+    //const [data, setData] = useState();
+    // const [searchTerm, setSearchTerm] = useState("");
 
 
-    var apitest = process.env.REACT_APP_RAPIDAPI_KEY;
-/*
-    var options = {
-        method: 'GET',
-        url: `https://exercisedb.p.rapidapi.com/exercises/name/${searchTerm}`, //use string template literals here or just put in urlHolder
-        headers: {
-            'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-            'x-rapidapi-key': apitest  //"123"
-        }
-    };
-*/
-    function work() {
-        /*
-        return axios.request(options).then(function (response) { //Response appears to be the argument to hold the actual returned data
-            //console.log(response.data);
-            setData(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
-        */
-       console.log("y")
-    }
-    
-
-    var allData = (data == "") ? "nothing" : data.map(value => <li>{value.name}</li>)
-
+    //var apitest = process.env.REACT_APP_RAPIDAPI_KEY;
     /*
-        function yes() {
-            console.log(data)
+        var options = {
+            method: 'GET',
+            url: `https://exercisedb.p.rapidapi.com/exercises/name/${searchTerm}`, //use string template literals here or just put in urlHolder
+            headers: {
+                'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+                'x-rapidapi-key': apitest  //"123"
+            }
+        };
+    
+        function work() {
+            
+            return axios.request(options).then(function (response) { //Response appears to be the argument to hold the actual returned data
+                //console.log(response.data);
+                setData(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+           
+           console.log("y")
         }
-    */
+        
+        */
+
+    var data = props.data;
+
+    var allData = (data === "") ? "nothing" : data.map(value => //Got to find some way to capitlize first word in name. Need an id too
+        <li> 
+            {value.name}
+        </li>
+        ) 
+
 
     return (
         <div>
-           <h3>Results List component</h3>
-            <button onClick={work} >Click</button>
-            
+            <h3>Results List component</h3>
+            {/*<button onClick={work} >Click</button> */}
+
             <ul>
                 {allData}
             </ul>
--- src/components/WorkoutApp.js
+++ src/components/WorkoutApp.js
@@ -1,21 +1,42 @@
 import React, { useState } from 'react';
 import ResultList from "./ResultList";
 import SearchForm from "./SearchForm";
+const axios = require('axios').default;
 
 
 function WorkoutApp(props) {
 
     const [value, setValue] = useState("");
+    const [result, setResult] = useState(""); //Data should be the returned api result
+
+    
 
     const handleChangeFunc = event => {
         setValue(event.target.value);
-        
     };
 
-
-    const handleSubmitFunc = (tester) => {
-        alert(value) 
+var urlHolder = `https://exercisedb.p.rapidapi.com/exercises/name/${value}` //Got to find some way to add  %20 into any white space in value
+var apiHolder = process.env.REACT_APP_RAPIDAPI_KEY;
+    
+    var options = {
+        method: 'GET',
+        url: urlHolder, //`https://exercisedb.p.rapidapi.com/exercises/name/${searchTerm}`, use string template literals here or just put in urlHolder
+        headers: {
+            'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+            'x-rapidapi-key': apiHolder  //"123"
+        }
     };
+
+const handleSubmitFunc = (tester) => {
+    return axios.request(options).then(function (response) { //Response appears to be the argument to hold the actual returned results
+        //console.log(response.result);
+        setResult( response.data);
+        console.log(result)
+    }).catch(function (error) {
+        console.error(error);
+    });
+    //alert(urlTest) 
+}
 
     return (
         <div>
@@ -25,7 +46,7 @@ function WorkoutApp(props) {
              handleSubmit={handleSubmitFunc}
              value={value}
             />
-            <ResultList />
+            <ResultList data ={result} />
             {/*<p>{value}</p>*/}
         </div>
     )
